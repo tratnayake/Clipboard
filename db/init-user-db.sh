a8cb856db72f68fb6647017f811d861af7aa3723
T Ratnayake <thilina.ratnayake1@gmail.com>
2016-12-03 14:29:26 -0800

    Initial Commit:
    - Set up a Docker environemt with NGINX, NODE & POSTGRES
    - All confirmed to be working

---
+++ db/init-user-db.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+    CREATE USER clipboard_docker;
+    CREATE DATABASE clipboard_docker;
+    GRANT ALL PRIVILEGES ON DATABASE clipboard_docker TO clipboard_docker;
+EOSQL
