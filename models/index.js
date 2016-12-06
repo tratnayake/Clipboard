afc50e9cd23f82043c9df543935e20c55dc1c2b4
T Ratnayake <thilina.ratnayake1@gmail.com>
2016-12-05 17:29:15 -0800

    Dev Migrations + User Password Hashing * Async/Await issue has been fixed and now app will come out and immediately add a test user + ranks * User model has been edited to include hashing the password as a beforeCreate hook

---
+++ models/index.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/*Dependencies*/
+
+let fs = require('fs');
+let path = require('path');
+let Sequelize = require('sequelize-values')();
+
+let sequelize = new Sequelize('postgres://' + process.env.DB_USERNAME + ':' + process.env.DB_PASSWORD + '@db:5432/' + process.env.DB_NAME, {
+	logging: false
+});
+
+
+let db = {};
+
+//For each file in the models directory, add the model
+//into memory.
+fs.readdirSync(__dirname).filter(function (file) {
+  return file.indexOf(".") !== 0 && file !== "index.js";
+}).forEach(function (file) {
+  var model = sequelize.import(path.join(__dirname, file));
+  db[model.name] = model;
+});
+
+Object.keys(db).forEach(function (modelName) {
+  if ("associate" in db[modelName]) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
---
+++ models/ranks.js
@@ -0,0 +1,20 @@
+"use strict";
+
+module.exports = function (sequelize, DataTypes) {
+  var Rank = sequelize.define("Rank", {
+    //Table columns 
+    number: { type: DataTypes.INTEGER, allowNull: false, unique: true },
+    name: { type: DataTypes.STRING, allowNull: false, unique: true },
+    longName: { type: DataTypes.STRING, allowNull: false, unique: true },
+    type: { type: DataTypes.ENUM('Officer', 'Cadet'), allowNull: false }
+  }, {
+    timestamps: false
+  }, {
+    getterMethods: {
+      abbreviation: function () {
+        return this.shortName;
+      }
+    }
+  });
+  return Rank;
+};
